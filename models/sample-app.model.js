162ca9226d89b3ca9fcc3ee053376f77cf3c37c7
Minh Vo <minhvo4398@gmail.com>
2024-05-15 12:11:37 +0700

    add test

---
+++ models/sample-app.model.js
@@ -0,0 +1,36 @@
+const { expect } = require("@playwright/test");
+
+exports.SampleAppPage = class SampleAppPage {
+  constructor(page) {
+    this.page = page;
+    this.userNameField = page.getByPlaceholder("User Name");
+    this.passwordField = page.getByPlaceholder("********");
+    this.loginButton = page.getByRole("button", { name: "Log In" });
+    this.logoutButton = page.getByRole("button", { name: "Log Out" });
+    this.loginText = page.locator("#loginstatus");
+  }
+
+  async navigateToSampleApp() {
+    await this.page.goto("http://uitestingplayground.com/sampleapp");
+  }
+
+  async fillUserNameField(username) {
+    await this.userNameField.fill(username);
+  }
+
+  async fillPasswordField(password) {
+    await this.passwordField.fill(password);
+  }
+
+  async clickLoginButton() {
+    await this.logoutButton.click();
+  }
+
+  async clickLoginButton() {
+    await this.logoutButton.click();
+  }
+
+  async expectLoginTextToBe(text) {
+    await expect(this.loginText).toHaveText(text);
+  }
+};
---
+++ tests/sample-app.spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+const { SampleAppPage } = require("../models/sample-app.model");
+
+test("log in success", async ({ page }) => {
+  const sampleAppPage = new SampleAppPage(page);
+  await sampleAppPage.navigateToSampleApp();
+  await sampleAppPage.fillUserNameField("Percival Picklepans");
+  await sampleAppPage.fillPasswordField("pwd");
+  await sampleAppPage.clickLoginButton();
+  await sampleAppPage.expectLoginTextToBe("Welcome, Percival Picklepans!");
+});
+test("Wrong password test", async ({ page }) => {
+  const sampleAppPage = new SampleAppPage(page);
+  await sampleAppPage.navigateToSampleApp();
+  await sampleAppPage.fillUserNameField("Gojo");
+  await sampleAppPage.fillPasswordField("WrongPass");
+  await sampleAppPage.clickLoginButton();
+  await sampleAppPage.expectLoginTextToBe("Invalid username/password");
+});
+
+test("No user name test", async ({ page }) => {
+  const sampleAppPage = new SampleAppPage(page);
+  await sampleAppPage.navigateToSampleApp();
+  await sampleAppPage.fillUserNameField("");
+  await sampleAppPage.fillPasswordField("pwd");
+  await sampleAppPage.clickLoginButton();
+  await sampleAppPage.expectLoginTextToBe("Invalid username/password");
+});
+
+test("Log out test", async ({ page }) => {
+  const sampleAppPage = new SampleAppPage(page);
+  await sampleAppPage.navigateToSampleApp();
+  await sampleAppPage.fillUserNameField("Gojo");
+  await sampleAppPage.fillPasswordField("pwd");
+  await sampleAppPage.expectLoginTextToBe("Welcome, Gojo!");
+  await sampleAppPage.clickLoginButton();
+  await sampleAppPage.expectLoginTextToBe("User logged out.");
+});
